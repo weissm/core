b1c6996f9017629f1dd3fb79ddce7f02f65c41e9
Franck Nijhof <git@frenck.dev>
2020-05-01 23:02:56 +0200

    Fire event when scenes are reloaded

--- tests/components/homeassistant/test_scene.py
+++ tests/components/homeassistant/test_scene.py
@@ -5,6 +5,7 @@ import pytest
 import voluptuous as vol
 
 from homeassistant.components.homeassistant import scene as ha_scene
+from homeassistant.components.homeassistant.scene import EVENT_SCENE_RELOADED
 from homeassistant.setup import async_setup_component
 
 from tests.common import async_mock_service
@@ -13,6 +14,11 @@ from tests.common import async_mock_service
 async def test_reload_config_service(hass):
     """Test the reload config service."""
     assert await async_setup_component(hass, "scene", {})
+
+    test_reloaded_event = []
+    hass.bus.async_listen(
+        EVENT_SCENE_RELOADED, lambda event: test_reloaded_event.append(event)
+    )
 
     with patch(
         "homeassistant.config.load_yaml_config_file",
@@ -23,6 +29,7 @@ async def test_reload_config_service(hass):
         await hass.async_block_till_done()
 
     assert hass.states.get("scene.hallo") is not None
+    assert len(test_reloaded_event) == 1
 
     with patch(
         "homeassistant.config.load_yaml_config_file",
@@ -32,6 +39,7 @@ async def test_reload_config_service(hass):
         await hass.services.async_call("scene", "reload", blocking=True)
         await hass.async_block_till_done()
 
+    assert len(test_reloaded_event) == 2
     assert hass.states.get("scene.hallo") is None
     assert hass.states.get("scene.bye") is not None
 
